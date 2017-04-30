521c618d0868ac8323458f984862ed45691ef62a
Woodcock <tw299@ic.ac.uk>
2017-04-30 14:19:52 +0100

    Cumulative time in ED plots improved.

--- R/ED_los_analysis.R
+++ R/ED_los_analysis.R
@@ -28,24 +28,43 @@ plot_ed_dist <- function(df, prov_codes = c("RBZ"), cumulative = TRUE) {
   if (nrow(prov_data) == 0) {return(NULL)}
   ed_dist <- aggregate(prov_data$Activity, by=list(Duration=prov_data$los_bin, Admitted=prov_data$admitted), FUN=sum)
   colnames(ed_dist)[3] <- "Frequency"
-  ed_dist[ed_dist$Admitted==FALSE,"cumul_sum"] <- cumsum(ed_dist[ed_dist$Admitted==FALSE,"Frequency"])
-  ed_dist[ed_dist$Admitted==TRUE,"cumul_sum"] <- cumsum(ed_dist[ed_dist$Admitted==TRUE,"Frequency"])
-  
+  if (cumulative) {
+    v <- cumsum(ed_dist[ed_dist$Admitted==FALSE,"Frequency"])
+    v1 <- v/max(v)*100
+    ed_dist[ed_dist$Admitted==FALSE,"Cumulative Percentage"] <- v1  
+    u <- cumsum(ed_dist[ed_dist$Admitted==TRUE,"Frequency"])
+    u1 <- u/max(u)*100
+    ed_dist[ed_dist$Admitted==TRUE,"Cumulative Percentage"] <- u1
+    w <- u + v
+    w1 <- w/max(w)*100
+    
+  }
   if(cumulative) {
     ed_dist_m <- reshape2::melt(ed_dist, id = c("Duration", "Admitted"))
-  } else {ed_dist_m <- ed_dist}
+    cm_df <- ed_dist_m[ed_dist_m$Admitted=="FALSE" & ed_dist_m$variable=="Cumulative Percentage",]
+    cm_df$value <- w1
+    cm_df$Admitted <- rep("ALL", length(w1))
+    ed_dist_m <- rbind(ed_dist_m, cm_df)
+    
+    intercepts_4h <- ed_dist_m[which(ed_dist_m$Duration=="[225,240)" & ed_dist_m$variable=="Cumulative Percentage"),which(colnames(ed_dist_m) %in% c("variable", "value", "Admitted"))]
+    
+  } else {
+    colnames(ed_dist)[3] <- "value"
+    ed_dist_m <- ed_dist
+  }
   
   pp <- ggplot(data = ed_dist_m, aes(x = Duration, y = value, group = Admitted, colour = Admitted))
-  pp <- pp + geom_path() + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+  pp <- pp + geom_path() + geom_point() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                                                axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), axis.title.y = element_blank())
   
   if(cumulative) {pp <- pp + facet_grid(variable ~ ., scales = "free")}
-  pp
+  pp + geom_vline(xintercept = 16, colour="grey60") + geom_hline(aes(yintercept = value, colour=Admitted), intercepts_4h) + ggtitle("Distribution of time in Emergency Department") + xlab("Time in ED")
     
 }
 
-make_adm_colour_palette <- function(levs) {
-  adm_colours <- RColorBrewer::brewer.pal(4,"Dark2")
-  names(adm_colours) <- append(levs, c(NA, NA), after = 1)
+make_adm_colour_palette <- function() {
+  adm_colours <- RColorBrewer::brewer.pal(8,"Dark2")
+  names(adm_colours) <- c("TRUE",NA,NA,"FALSE",NA,NA,NA,"ALL")
   admColScale <- scale_colour_manual(name="Admitted", values = adm_colours)
   admColScale
 }
