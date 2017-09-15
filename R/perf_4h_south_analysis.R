ccd2ddef4f8fc215107c93093a9f082e4eb08245
HorridTom <woodcock.thomas@gmail.com>
2017-09-15 10:31:04 +0100

    Added function to plot SPC charts for volume.

--- R/perf_4h_south_analysis.R
+++ R/perf_4h_south_analysis.R
@@ -163,3 +163,66 @@ plot_volume <- function(df, prov_codes = c("RBZ"), date.col = 'Wk_End_Sun',
   
 }
 
+
+plot_volume_c <- function(df, prov_codes = c("RBZ"), date.col = 'Wk_End_Sun',
+                             start.date = "2014-01-01", end.date = "2017-02-28",
+                             brk.date = "2016-01-01", min_upper_y_scale = 3000,
+                             adm_only = FALSE, all_provs = FALSE,
+                             dept_types = c('1','2','3'), plot.chart = TRUE,
+                             pr_name = NULL) {
+  # pass df as cleaned 4h perf data from the clean_4h_data function
+  
+  # if no pr_name passed, lookup full name of provider
+  # note written for just one provider
+  if (is_null(pr_name)) {
+    pr_name <- df[which(df$Prov_Code == prov_codes),"Prov_Name"][[1]]
+  }
+  #cht_title = paste("Weekly percentage ED attendances with time in department < 4h",pr_name,sep="\n")
+  if (adm_only) {
+    cht_title = paste("Weekly total admissions through ED. Dept. types: ",paste(dept_types,sep="",collapse=","),sep="")
+    y_axis_lab = "Admissions"
+  } else {
+    cht_title = paste("Weekly total ED attendances. Dept. types: ",paste(dept_types,sep="",collapse=","),sep="")
+    y_axis_lab = "Attendances"
+  }
+  
+  df <- make_perf_series(df = df, prov_codes = prov_codes, adm_only = adm_only,
+                         all_provs = all_provs, dept_types = dept_types)
+  
+  st.dt <- as.Date(start.date)
+  ed.dt <- as.Date(end.date)
+  br.dt <- as.Date(brk.date)
+  
+  # restrict to the period specified
+  df <- df[df[,date.col] >= st.dt & df[,date.col] <= ed.dt,]
+  
+  # locate break row
+  v <- df[,date.col]
+  br.row <- which(v == max(v[v < br.dt]))
+  
+  # This is a hack - find better way to modify colours of qicharts
+  # Also needs stepped limits
+  
+  pct <- qicharts::tcc(n = Total, d = rep(1, nrow(df)), x = df$Wk_End_Sun, data = df, chart = 'u', prime = TRUE, breaks = c(br.row), runvals = TRUE, cl.lab = TRUE)
+  
+  # chart y limit
+  ylimhigh <- max(max(df$Total),min_upper_y_scale)
+  
+  col1    <- rgb(000, 000, 000, maxColorValue = 255)
+  col2    <- rgb(241, 088, 084, maxColorValue = 255)
+  col3    <- rgb(000, 000, 000, maxColorValue = 255)
+  col4    <- 'white'
+  col5    <-  rgb(096, 189, 104, maxColorValue = 255)
+  cols    <- c('col1' = col1, 'col2' = col2, 'col3' = col3, 'col4' = col4)
+  
+  if(plot.chart == TRUE) {pct + geom_line(aes_string(x = 'x', y = 'lcl', group = 'breaks'), colour = '#000000', linetype = 'dashed') +
+      geom_line(aes_string(x = 'x', y = 'ucl', group = 'breaks'), colour = '#000000', linetype = 'dashed') +
+      geom_line(aes_string(x = 'x', y = 'cl', group = 'breaks'), colour = '#000000', linetype = 1) +
+      geom_line(aes_string(x = 'x', y = 'y', group = 'breaks'), colour = '#000000', linetype = 1, lwd = 1.1) + 
+      geom_point(aes_string(x = 'x', y = 'y', group = 'breaks', fill = 'pcol'), size = 2) + 
+      scale_fill_manual(values = cols) + scale_color_manual(values = cols) +
+      labs(title = cht_title, x="Week Ending Sunday", y=y_axis_lab, subtitle = pr_name) +
+      ylim(0,ylimhigh) + scale_x_date(labels = date_format("%Y-%m"), breaks = date_breaks("3 months"), limits = as.Date(c(start.date, end.date))) + theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 0.75), plot.title = element_text(hjust = 0.5), axis.line = element_line(colour = "grey60"))
+  } else {df}
+  
+}
