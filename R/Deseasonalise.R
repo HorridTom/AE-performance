14fb90ba0ecc86f089e8456e73bac5cfd860f987
HorridTom <woodcock.thomas@gmail.com>
2017-09-16 16:56:36 +0100

    Added functions to compute seasonal factors, and perform overall deseasonalisation.

--- R/Deseasonalise.R
+++ R/Deseasonalise.R
@@ -1,4 +1,9 @@
-make_seasonal_relatives <- function(df, season_col, period_col,
+library(tidyverse)
+library(zoo)
+library(bsts)
+
+
+make_seasonal_relatives <- function(df, period_col,
                                baseline_periods = NULL) {
   
   period_col <- enquo(period_col)
@@ -18,4 +23,42 @@ make_seasonal_relatives <- function(df, season_col, period_col,
   
   df %>% mutate(seasonal_relative = Performance/Period_Av)
   
+}
+
+
+get_seasonal_factors <- function(df, season_col) {
+  
+  season_col <- enquo(season_col)
+  
+  df %>% group_by(!!season_col) %>% summarise(seasonal_factor = mean(seasonal_relative, na.rm = TRUE))
+  
+}
+
+
+deseasonalise <- function(df, season_col, period_col, baseline_periods = NULL) {
+  
+  df <- make_seasonal_relatives(df = df, period_col = !!enquo(period_col), baseline_periods = baseline_periods)
+  sf <- get_seasonal_factors(df = df, season_col = !!enquo(season_col))
+  
+  df <- df %>% left_join(sf)
+  
+  df %>% mutate(Deseasonalised_Performance = Performance / seasonal_factor)
+  
+}
+
+
+convert_weekly_to_monthly <- function(df, date_col, measure_col) {
+  
+  # First make two zoo objects, one for the weekly measure and one to count weeks in the month
+  mps <- read.zoo(df[,which(colnames(df) %in% c(date_col,measure_col))])
+  df$wc <- rep(1,nrow(df))
+  mps_wc <- read.zoo(df[,which(colnames(df) %in% c(date_col,"wc"))])
+  
+  # Create monthly series, aportioning weeks across month boundaries linearly
+  monthly_sums <- AggregateWeeksToMonths(weekly.series = mps)
+  num_weeks <- AggregateWeeksToMonths(weekly.series = mps_wc)
+  monthly_measure <- monthly_sums/num_weeks
+
+  mdf <- data.frame(month = index(monthly_measure), as.data.frame(monthly_measure))
+  mdf
 }
