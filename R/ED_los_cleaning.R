6d2e535ee64dd0efe1825aee804fa91b6165f99d
HorridTom <woodcock.thomas@gmail.com>
2017-11-23 12:47:28 +0000

    Improved automation of data import.
    Now pulls from any number of appropriately-named files, renames columns, and corrects boolean value coding.

--- R/ED_los_cleaning.R
+++ R/ED_los_cleaning.R
@@ -2,7 +2,7 @@
 clean_los_data <- function(df, sv = TRUE, rt = FALSE, disp_propmiss = TRUE) {
   
   # Convert the following columns to numeric
-  df[,"Der_AEA_Duration"] <- as.integer(levels(df[,"Der_AEA_Duration"]))[df[,"Der_AEA_Duration"]]
+  df[,"Der_AEA_Duration"] <- as.integer(df[,"Der_AEA_Duration"])
   
   # Convert the following columns to character
   df[,"Provider_Name"] <- as.character(df[,"Provider_Name"])
